b5aa76ab618238b2022910a22346fae72afed37d
Jane <tanhuiqing95@hotmail.com>
2024-05-09 11:08:44 +0800

    feat add conditional render AuthPage

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,13 +1,29 @@
 import debug from "debug";
+import { useState } from "react";
+import AuthPage from "../AuthPage/AuthPage";
+import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
 
 const log = debug("mern:pages:App:App");
-localStorage.debug = "mern:*";
 
 function App() {
-  log("inside App")
+  // eslint-disable-next-line no-unused-vars
+  const [user, setUser] = useState();
+  log("user %o", user);
+
+  if (!user) {
+    return (
+      <main className="App">
+        <AuthPage />
+      </main>
+    );
+  }
+
   return (
     <>
       <main className="App">App</main>
+      <NewOrderPage />
+      <OrderHistoryPage />
     </>
   );
 }
